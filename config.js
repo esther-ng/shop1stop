ff7d74eca86d61420b0d0d70975768f4a1f52716
Esther Ng <esther.hc.ng@gmail.com>
2017-01-23 18:15:10 -0800

    Remove config

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  API_KEY: 'AIzaSyBubrZfeGEvTisf-fdJMdEXcHYdhwF9Adk',
-  AUTH_DOMAIN: 'shop1stop-bee2e.firebaseapp.com',
-  DATABASE_URL: 'https://shop1stop-bee2e.firebaseio.com',
-  STORAGE_BUCKET: 'shop1stop-bee2e.appspot.com'
-};
